2a7d8fe72b8304c5dac9122ed91938a2efeed440
Tejas <tejasspatell2@gmail.com>
2025-11-08 23:57:09 +0530

    modify content

--- Frontend/src/Components/Itjobpeperation.jsx
+++ Frontend/src/Components/Itjobpeperation.jsx
@@ -9,72 +9,76 @@ export default function Itjobpeperation() {
   return (
     <section className="career-dashboard">
       <div className="it-section-title">
-      <div className="float-icon-containerr">
-        <img
-          src="people.png"
-          alt="Floating Icon"
-          className="floating-icon-preperation"
-        />
-      </div>
-      <h2>Your Career Dashboard</h2>
-      <p>All your tools, insights, and goals — in one place. Track progress like a pro.</p>
-      </div>
-
-        <div className="dashboard-grid">
-          <div className="dashboard-widget">
-            <div className="preparation-icon">
-              <FaPuzzlePiece />
-            </div>
-            <h3 className="widget-title">IT Role Matcher</h3>
-            <p className="widget-text">
-              Not sure where to start? Explore how your skills fit into today’s top tech careers — and uncover personalized paths for future-ready IT journey.
-            </p>
-            <NavLink to="/it-jobs/ITJobSection-platform" className="widget-btn">Explore</NavLink>
-          </div>
-
-          <div className="dashboard-widget">
-            <div className="preparation-icon">
-              <LuRoute />
-            </div>
-            <h3 className="widget-title">
-              Jobs Roadmap
-            </h3>
-            <p className="widget-text">
-              Discover the ultimate roadmap for IT jobs — covering every essential skill, tool, and step you need to build a successful tech career.
-            </p>
-
-            <NavLink to="/it-jobs/practical-roadmap" className="widget-btn">Explore</NavLink>
-          </div>
-
-          <div className="dashboard-widget">
-            <div className="preparation-icon">
-              <FaBriefcase />
-            </div>
-
-            <h3 className="widget-title">
-              Top Hiring Platforms
-            </h3>
-            <p className="widget-text">
-              Ready to get hired? Explore top tech jobs across India’s best platforms — faster, smarter, and all in one powerful platform.
-            </p>
-            <NavLink to="/it-jobs/ITjobsearch-platform" className="widget-btn">Explore</NavLink>
-          </div>
-
-          <div className="dashboard-widget">
-             <div className="preparation-icon">
-              <FaCodeBranch />
-            </div>
-            
-            <h3 className="widget-title">IT Job Resources</h3>
-            <p className="widget-text">
-              Explore top platforms for resume building, coding practice, aptitude prep, mock interviews, and open-source contributions.
-            </p>
-            <NavLink to="/it-jobs/ITJobresources-platform" className="widget-btn">
-              Explore
-            </NavLink>
-          </div>
-
+        <div className="float-icon-containerr">
+          <img
+            src="people.png"
+            alt="Floating Icon"
+            className="floating-icon-preperation"
+          />
         </div>
+        <h2>Your Career Dashboard</h2>
+        <p>All your tools, insights, and goals — in one place. Track progress like a pro.</p>
+      </div>
+
+      <div className="dashboard-grid">
+        <div className="dashboard-widget">
+          <div className="it-preparation-icon">
+            <FaPuzzlePiece />
+          </div>
+          <h3 className="widget-title">IT Role Matcher</h3>
+          <p className="widget-text">
+            Unsure where to start? Match your skills with top tech roles and find your perfect IT path.
+          </p>
+
+          <NavLink to="/it-jobs/ITJobSection-platform" className="widget-btn">Explore</NavLink>
+        </div>
+
+        <div className="dashboard-widget">
+          <div className="it-preparation-icon">
+            <LuRoute />
+          </div>
+          <h3 className="widget-title">
+            Jobs Roadmap
+          </h3>
+          <p className="widget-text">
+            Follow a clear roadmap with key skills and tools to grow your IT career step by step.
+          </p>
+
+
+          <NavLink to="/it-jobs/practical-roadmap" className="widget-btn">Explore</NavLink>
+        </div>
+
+        <div className="dashboard-widget">
+          <div className="it-preparation-icon">
+            <FaBriefcase />
+          </div>
+
+          <h3 className="widget-title">
+            Top Hiring Platforms
+          </h3>
+          <p className="widget-text">
+            Explore top tech jobs from India’s best platforms — faster, smarter, all in one place.
+          </p>
+
+          <NavLink to="/it-jobs/ITjobsearch-platform" className="widget-btn">Explore</NavLink>
+        </div>
+
+        <div className="dashboard-widget">
+          <div className="it-preparation-icon">
+            <FaCodeBranch />
+          </div>
+
+          <h3 className="widget-title">IT Job Resources</h3>
+          <p className="widget-text">
+            Access the best tools for resumes, coding, aptitude, interviews, and open-source work.
+          </p>
+
+          <NavLink to="/it-jobs/ITJobresources-platform" className="widget-btn">
+            Explore
+          </NavLink>
+        </div>
+
+      </div>
     </section>
   )
 }
