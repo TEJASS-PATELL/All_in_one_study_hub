4d283aa4388c05d03fac7d17de086690833e4c9a
Tejas <tejasspatell2@gmail.com>
2025-11-18 01:17:03 +0530

    modify backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,7 +7,11 @@ const passport = require("./config/passport");
 const app = express();
 dotenv.config();
 
-app.use(cors({ origin: [ "http://localhost:5173", process.env.CLIENT_URL ], credentials: true }));
+app.use(cors({
+  origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize()); 
