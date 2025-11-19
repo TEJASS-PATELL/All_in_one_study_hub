25cd628605d59390cceeee8859690ed44c44bf26
Tejas <tejasspatell2@gmail.com>
2025-11-19 16:18:03 +0530

    fix deployment error

--- Backend/src/config/nodemailer.js
+++ Backend/src/config/nodemailer.js
@@ -1,11 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com", 
+    port: 465, 
+    secure: true, 
     auth: {
         user: process.env.GMAIL_USER, 
         pass: process.env.GMAIL_PASS, 
     },
-})
+});
+
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log("❌ SMTP Connection Error:", error);
+    } else {
+        console.log("✅ Server is ready to take our messages!");
+    }
+});
+
 
 module.exports = transporter;
