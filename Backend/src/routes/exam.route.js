98c806748bea4ea0ad58fae98928c4e777546b02
Tejas <tejasspatell2@gmail.com>
2025-11-21 23:12:37 +0530

    fix some errors

--- Backend/src/routes/exam.route.js
+++ Backend/src/routes/exam.route.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
 const { getexamdata } = require("../controllers/exam.controller");
 const { getprivatejobdata } = require("../controllers/privatejob.controller");
-const authentication = require("../middlewares/auth.middleware");
-const prisma = new PrismaClient();
 
 router.get("/government-jobs", getexamdata);
 router.get("/private-jobs", getprivatejobdata);
