01c7314f9ba4c863a1d6e307f2a64d32f90915ad
Tejas <tejasspatell2@gmail.com>
2025-11-30 22:15:31 +0530

    convert into es6 module and correct some error

--- Backend/src/routes/chatbot.route.js
+++ Backend/src/routes/chatbot.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import chatbot from "../controllers/chatbot.controller.js";
+import { chatbot } from "../controllers/chatbot.controller.js";
 const router = express.Router();
 
 router.post("/chatbot", chatbot);
