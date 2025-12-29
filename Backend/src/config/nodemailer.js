21eeacaede43895d6336fe280968f88369248275
Tejas <tejasspatell2@gmail.com>
2025-12-29 21:16:16 +0530

    fix email service

--- Backend/src/config/nodemailer.js
+++ Backend/src/config/nodemailer.js
@@ -2,15 +2,16 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465, 
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
+  tls: {
+    rejectUnauthorized: false 
+  },
+  connectionTimeout: 20000, 
 });
 
 transporter.verify((error, success) => {
