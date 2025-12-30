c2c1ca0d1121ee415c42df10f5e037d859fda4df
Tejas <tejasspatell2@gmail.com>
2025-12-30 19:58:23 +0530

    add chat-room feature

--- Frontend/src/Components/EnterEmail.jsx
+++ Frontend/src/Components/EnterEmail.jsx
@@ -9,13 +9,13 @@ export default function EnterEmail() {
   const [email, setemail] = useState("");
   const { sendPasswordResetLink, isLoading } = useAuthStore();
   const navigate = useNavigate();
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!/\S+@\S+\.\S+/.test(email)) return toast.error("Invalid email format");
 
     const result = await sendPasswordResetLink(email);
-    if(result){
+    if (result) {
       navigate("/login");
     }
     else return false;
@@ -23,32 +23,36 @@ export default function EnterEmail() {
 
   return (
     <div className="auth-container">
-    <div className="forgot-form">
-      <h1>Forgot Password</h1>
-      <p className="forgot-desc">
-        Enter your email below and we'll send you a 6-digit code to reset your password.
-      </p>
-      <div className="forgot-div">
-        <input
-          value={email}
-          onChange={(e) => setemail(e.target.value)}
-          id="email"
-          type="email"
-          placeholder="Enter your email"
-          required
-        />
-        <button onClick={handleSubmit} type="submit" className="forgot-btn">
-          {isLoading ? (
-                <>
-                  <Loader2 className="loader animate-spin mr-2" size={18} />
-                  Sending Link...
-                </>
-              ) : (
-                "Send Reset Code"
-              )}
-        </button>
+      <div className="forgot-form">
+        <h1>Forgot Password</h1>
+        <p>
+          Enter your email address below, and we'll send you a secure <b>link</b> to <b>reset password</b>.
+        </p>
+        <div className="forgot-div">
+          <input
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            required
+          />
+          <button onClick={handleSubmit} type="submit" className="forgot-btn">
+            {isLoading ? (
+              <>
+                <Loader2 className="loader animate-spin mr-2" size={18} />
+                Sending Link...
+              </>
+            ) : (
+              "Send Reset Link"
+            )}
+          </button>
+
+          <p className="spam-notice">
+            <b>Note:</b> If you don't see the email in your inbox, please check your <b>Spam or Junk folder</b>.
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
