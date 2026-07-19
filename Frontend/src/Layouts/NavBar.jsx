922ac835e29775b752589b4e1d1fd2cca714c593
Tejas <tejasspatell2@gmail.com>
2026-07-20 02:15:19 +0530

    changing the design modify UI

--- Frontend/src/Layouts/NavBar.jsx
+++ Frontend/src/Layouts/NavBar.jsx
@@ -11,27 +11,29 @@ export default function NavBar() {
   return (
     <nav className="Lnavbar">
       <div className='Lnavbar-container'>
+        
         <div className="Lnav-logo" onClick={() => navigate("/")}>
           ALL-IN<span>-ONE</span>
         </div>
 
         <ul className="Lnavbar-links">
-          <li><Link to="/government-jobs/Study-Material" className="nav-btn-link">Prep-Resources</Link></li>
-          <li><Link className="ai-tools" to="/ai-tools">AI-Tools</Link></li>
-          <li><Link to="/it-jobs/ITjobsearch-platform">Job-Search</Link></li>
-          <li><Link to="/dashboard/roadmap">RoadMap</Link></li>
-          <li><Link to="/dashboard/chatroom" className="nav-btn-link">Chat-Room</Link></li>
+          <li><Link to="/government-jobs/Study-Material" className="nav-link">Prep-Resources</Link></li>
+          <li><Link to="/ai-tools" className="nav-link ai-tools">AI-Tools</Link></li>
+          <li><Link to="/it-jobs/ITjobsearch-platform" className="nav-link">Job-Search</Link></li>
+          <li><Link to="/dashboard/roadmap" className="nav-link">RoadMap</Link></li>
+          <li><Link to="/dashboard/chatroom" className="nav-link">Chat-Room</Link></li>
         </ul>
 
         <div className="Lnavbar-auth-buttons">
           {authUser ? (
-            <div className="Lprofile-wrapper" onClick={() => navigate("/dashboard")}>
-              <FaUserCircle size={38} />
+            <div className="Lprofile-wrapper" onClick={() => navigate("/dashboard")} title="Dashboard">
+              <FaUserCircle className="nav-profile-icon" />
             </div>
           ) : (
             <Link to="/login" className="Llogin-btn">Login</Link>
           )}
         </div>
+
       </div>
     </nav>
   );
