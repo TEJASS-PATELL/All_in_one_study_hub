7d80a8badd3cebd82a168abdd1884e8903d1cd5d
Tejas <tejasspatell2@gmail.com>
2026-07-20 02:14:55 +0530

    changing the design modify UI

--- Frontend/src/Layouts/Footer.jsx
+++ Frontend/src/Layouts/Footer.jsx
@@ -6,42 +6,43 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className="footer-container">
-        <div className="footer-bottom">
-          <div className="social-links">
-            <a
-              href="https://mail.google.com/mail/?view=cm&to=tejasspatell2@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="social-link"
-              title="Email"
-            >
-              <FaEnvelope />
-            </a>
+        
+        <p className="copyright">
+          © {new Date().getFullYear()} Career Hub by <span>Tejas Patel</span> | All rights reserved
+        </p>
 
-            <a 
-              href="https://linkedin.com/in/tejasspatell"
-              className="social-link"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="LinkedIn"
-            >
-              <FaLinkedinIn />
-            </a>
+        <div className="social-links">
+          <a
+            href="https://mail.google.com/mail/?view=cm&to=tejasspatell2@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+            title="Email"
+          >
+            <FaEnvelope />
+          </a>
 
-            <a 
-              href="https://github.com/TEJASS-PATELL"
-              className="social-link"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="GitHub"
-            >
-              <FaGithub />
-            </a>
-          </div>
-          <p className="copyright">
-            © 2025 Career Hub by <span>Tejas PateL</span> | All rights reserved
-          </p>
+          <a 
+            href="https://linkedin.com/in/tejasspatell"
+            className="social-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="LinkedIn"
+          >
+            <FaLinkedinIn />
+          </a>
+
+          <a 
+            href="https://github.com/TEJASS-PATELL"
+            className="social-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub"
+          >
+            <FaGithub />
+          </a>
         </div>
+        
       </div>
     </footer>
   );
