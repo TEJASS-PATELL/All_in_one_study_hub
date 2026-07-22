14b4d95311bc6f190a479e4c93dd5d1be83f2a4a
Tejas <tejasspatell2@gmail.com>
2026-07-22 23:17:19 +0530

    changing the design modify UI

--- Frontend/src/Components/Private/PrivateExams.jsx
+++ Frontend/src/Components/Private/PrivateExams.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './PrivateJobs.css';
 import '../Government/Exam.css';
-import Loading from '../../layouts/Loading';
+import Loading from '../../layouts/Home/Loading';
 
 const PrivateExams = () => {
   const [activeCategory, setActiveCategory] = useState(null);
